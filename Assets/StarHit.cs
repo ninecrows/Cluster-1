de4246c87dc9d6543a291b5e1129b9ea24ba6c2a
Kyle Wilson <kyle@ninecrows.com>
2019-03-09 23:06:13 -0500

    Added Haptic Feedback (finally). Most of the videos failed to provide up to date information. Nice to have the controller react when it intersects an interaction target.

--- Assets/StarHit.cs
+++ Assets/StarHit.cs
@@ -7,6 +7,26 @@ using Valve.VR.InteractionSystem;
 public class StarHit : MonoBehaviour
 {
     public GameObject selectionMarker = null;
+    public SteamVR_Action_Vibration hapticAction;
+    public SteamVR_Action_Boolean trackPadAction;
+
+    private void Update()
+    {
+        if (trackPadAction.GetStateDown(SteamVR_Input_Sources.LeftHand))
+        {
+            Pulse(1, 150, 75, SteamVR_Input_Sources.LeftHand);
+        }
+
+        if (trackPadAction.GetStateDown(SteamVR_Input_Sources.RightHand))
+        {
+            Pulse(1, 150, 75, SteamVR_Input_Sources.RightHand);
+        }
+    }
+
+    private void Pulse(float duration, float frequency, float amplitude, SteamVR_Input_Sources source)
+    {
+        hapticAction.Execute(0, duration, frequency, amplitude, source);
+    }
 
     private void OnTriggerEnter(Collider other)
     {         
@@ -18,7 +38,12 @@ public class StarHit : MonoBehaviour
 
             GameObject selector = Instantiate(selectionMarker, other.transform);
 
+            //CVRSystem.TriggerHapticPulse(0, 0, 100);  //HandRole.LeftHand);
+
+            //ViveInput.TriggerHapticPulse(HandRole.LeftHand);
             //other.transform
+
+            Pulse(0.05f, 800, 25, SteamVR_Input_Sources.RightHand);
         }
     }
 }
